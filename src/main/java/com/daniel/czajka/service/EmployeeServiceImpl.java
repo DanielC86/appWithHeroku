da366639224b2282cdfa489055ddd5e3837e8d14
Daniel <daniel.czajka@gmail.com>
2020-03-06 10:08:57 +0100

    adding service layer for employee

---
+++ src/main/java/com/daniel/czajka/service/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.daniel.czajka.service;
+
+import com.daniel.czajka.dao.EmployeeDAO;
+import com.daniel.czajka.dao.EmployeeDAOImpl;
+import com.daniel.czajka.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO){
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Employee findById(int theEmployeeId) {
+        return employeeDAO.findById(theEmployeeId);
+    }
+
+    @Override
+    @Transactional
+    public void save(Employee theEmployee) {
+        employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theEmployeeId) {
+        employeeDAO.deleteById(theEmployeeId);
+    }
+}
