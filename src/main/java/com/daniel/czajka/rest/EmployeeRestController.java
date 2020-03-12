f499d480a036d4dc1c9f24518afc2151b702eecf
Daniel Czajka <daniel.czajka@gmail.com>
2020-03-12 21:17:02 +0100

    starting working on front end

--- src/main/java/com/daniel/czajka/rest/EmployeeRestController.java
+++ src/main/java/com/daniel/czajka/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.daniel.czajka.rest;
 import com.daniel.czajka.entity.Employee;
 import com.daniel.czajka.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,21 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+
+    //trying to get the model for website
+    @GetMapping("/list")
+    public String employeeList(Model theModel) {
+
+        //get the data from database
+        List<Employee> allEmployees = employeeService.findAll();
+
+        //add data to the model
+        theModel.addAttribute("employees", allEmployees);
+
+        //return the data on the website
+        return "employeesList";
+
     }
 }
